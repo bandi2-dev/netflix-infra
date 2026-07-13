eeae837eb210f7a578e12c037295b4f10b4d69dc
bandi2-dev <cloudd3vops@gmail.com>
2026-07-13 05:13:45 +0000

    Provision GCP networking foundation with Terraform

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -33,3 +33,69 @@ resource "google_compute_subnetwork" "subnet" {
   }
 
 }
+
+resource "google_compute_firewall" "allow_internal" {
+
+  name    = "allow-internal-${var.environment}"
+  project = var.project_id
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = [
+    "10.10.0.0/24",
+    "10.20.0.0/16",
+    "10.30.0.0/20"
+  ]
+
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+
+  name    = "allow-ssh-${var.environment}"
+  project = var.project_id
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+
+}
+
+resource "google_compute_router" "router" {
+
+  name    = "netflix-router-${var.environment}"
+  region  = var.region
+  project = var.project_id
+  network = google_compute_network.vpc.id
+
+}
+
+resource "google_compute_router_nat" "nat" {
+
+  name   = "netflix-nat-${var.environment}"
+  router = google_compute_router.router.name
+
+  region = var.region
+  project = var.project_id
+
+  nat_ip_allocate_option = "AUTO_ONLY"
+
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+}
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -13,3 +13,11 @@ output "subnet_name" {
 output "subnet_self_link" {
   value = google_compute_subnetwork.subnet.self_link
 }
+
+output "router_name" {
+  value = google_compute_router.router.name
+}
+
+output "nat_name" {
+  value = google_compute_router_nat.nat.name
+}
